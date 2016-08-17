a29637e49a9cfdd2c1c43cb86750341e0fd6ecb5
isachse <danielsachse@lucie-macbook-pro.x-case.local>
2016-08-17 18:00:20 +0200

    rework

--- server/auth.js
+++ server/auth.js
@@ -1,6 +1,6 @@
 var basicAuth       = require('basic-auth');
 
-export.checkAuth = function (req, res, next) {
+exports.checkAuth = function (req, res, next) {
   function unauthorized(res) {
     res.set('WWW-Authenticate', 'Basic realm=Authorization Required');
     return res.send(401);
