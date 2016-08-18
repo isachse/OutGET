f3f13cbed77c6df9e99e49a305a01eec2fbd9519
isachse <danielsachse@lucie-macbook-pro.x-case.local>
2016-08-18 06:50:21 +0200

    Remove unused code

--- server/server.js
+++ server/server.js
@@ -11,15 +11,6 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(methodOverride());      // simulate DELETE and PUT
 
-/*
-// CORS (Cross-Origin Resource Sharing) headers to support Cross-site HTTP requests
-app.all('*', function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    next();
-});
-*/
-
 app.get('/exec/:cmd', auth.checkAuth, exec.cmd);
 app.get('/ping', auth.checkAuth, exec.ping);
 app.get('/nslookup', auth.checkAuth, exec.nslookup);
