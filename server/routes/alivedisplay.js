da824b8cc15569c2114f40f2c1fff05411d852cd
isachse <danielsachse@lucie-macbook-pro.x-case.local>
2017-01-07 17:50:23 +0100

    add persistence

--- server/routes/alivedisplay.js
+++ server/routes/alivedisplay.js
@@ -1,6 +1,19 @@
-require('shelljs/global');
+
 
 exports.setState = function (req, res, next) {
+
+    var jsonfile = require('jsonfile');
+
+    var displayid = req.params.displayid;
+     
+    var file = concat('/tmp/',displayid,'.json');
+    var obj = {name: 'JP'};
+     
+    jsonfile.writeFile(file, obj, function (err) {
+      console.error(err);
+    })
+
+
     res.statusCode = 200;
     res.send();
 };
@@ -13,7 +26,14 @@ exports.getState = function (req, res, next) {
         res.set('Content-Type', 'image/png');
         res.send("");
     } else {
+
+        var jsonfile = require('jsonfile');
+
+        var displayid = req.params.displayid;
+         
+        var file = concat('/tmp/',displayid,'.json');
+ 
         res.set('Content-Type', 'application/json');
-        res.send();        
+        res.send(jsonfile.readFileSync(file));        
     };
 }
