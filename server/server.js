351a712dec8d0cdfe986702a8d83275f866cf12b
isachse <daniel.sachse@isachse.com>
2017-01-08 09:14:57 -0500

    fix

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 var auth            = require('./auth'),
-	alivedisplay	= require('./routes/alivedisplay'),	
+    alivedisplay    = require('./routes/alivedisplay'),
     bodyParser      = require('body-parser'),
     exec            = require('./routes/exec'),
     express         = require('express'),
@@ -12,11 +12,11 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(methodOverride());      // simulate DELETE and PUT
 
-app.post('/state/:displayid', auth.checkAuth, alivedisplay.setState);
-app.get('/state/:displayid', auth.checkAuth, alivedisplay.getState);
-app.get('/exec/:cmd', auth.checkAuth, exec.cmd);
-app.get('/ping', auth.checkAuth, exec.ping);
-app.get('/nslookup', auth.checkAuth, exec.nslookup);
+app.post('/alivedisplay/:displayid/state', auth.checkAuth, alivedisplay.setState);
+app.get('/alivedisplay/:displayid/state', auth.checkAuth, alivedisplay.getState);
+app.get('/outget/exec/:cmd', auth.checkAuth, exec.cmd);
+app.get('/outget/ping', auth.checkAuth, exec.ping);
+app.get('/outget/nslookup', auth.checkAuth, exec.nslookup);
 
 app.set('port', process.env.PORT || 80);
 
